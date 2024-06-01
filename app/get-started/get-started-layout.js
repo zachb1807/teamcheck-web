215c8627e27c01a1824163e04f416a606550e227
Zach Buchar - Envy <zach@zachnology.com>
2024-06-01 16:20:10 -0500

    Reword sign in page

--- app/get-started/get-started-layout.js
+++ app/get-started/get-started-layout.js
@@ -17,9 +17,9 @@ export default function GetStartedLayout({client_id, redirect_uri}) {
                     <Container centerContent maxW='container.md' className="mt-16">
                         <Image src="/teamsnap.svg" alt="TeamSnap logo" className="h-11"></Image>
                         <Box height={`51`}></Box>
-                        <Heading variant="disable_font_center" color="gray.700">Authorize your TeamSnap account to continue</Heading>
+                        <Heading variant="disable_font_center" color="gray.700">Sign in with TeamSnap.com to continue</Heading>
                         <Box height={`51`}></Box>
-                        <Text align="center" fontSize='xl' color="gray.700">We&apos;re going to redirect you to TeamSnap to connect your account. Please allow all permissions to ensure TeamCheck works properly.</Text>
+                        <Text align="center" fontSize='xl' color="gray.700">We&apos;re going to redirect you to the TeamSnap website to connect your account. Please allow all permissions to ensure TeamCheck works properly.</Text>
                         <Box height={`51`}></Box>
                         <Link href={"https://auth.teamsnap.com/oauth/authorize?authorization_test=true&client_id=" + client_id + "&redirect_uri=" + redirect_uri + "&response_type=code&scope=write%20read"}>
                             <Button colorScheme="teal">Sign In<ExternalLinkIcon className={`ml-2`} /></Button>
